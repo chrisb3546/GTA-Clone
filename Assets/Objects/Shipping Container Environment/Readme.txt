693b5ef0cac8c846410dac8d7b8c8ec16526d1e9
chrisb3546 <chrisb3546@yahoo.com>
2020-11-10 19:05:15 -0500

    add camera rotation script, import and add prefabs for opening scene

---
+++ Assets/Objects/Shipping Container Environment/Readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f3a43c9c406e7cedebf2378ff92932c36ec2b6784813812b5f0fb05987132e90
+size 885
---
+++ Assets/Scripts/Characters/CamControl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8890980f0ab2943976d5b375d452ab8784bfb1d6ea5ccfebe36434ebac250c44
+size 439
