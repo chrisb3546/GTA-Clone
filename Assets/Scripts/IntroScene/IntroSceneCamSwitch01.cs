3994beacb1f41ac8e7b8d68250cb293319c3a580
chrisb3546 <chrisb3546@yahoo.com>
2020-11-12 11:17:34 -0500

    add sound effect

--- Assets/Scripts/IntroScene/IntroSceneCamSwitch01.cs
+++ Assets/Scripts/IntroScene/IntroSceneCamSwitch01.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c75057151503c3bce4e14bf8f0a1992565cb08e5169b5482bfdc0aec81117dd6
-size 602
+oid sha256:17f1ae55e76fbfd8870e5bcbc11f1bc8c20475c9eb2992c10e60d9d362789974
+size 606
