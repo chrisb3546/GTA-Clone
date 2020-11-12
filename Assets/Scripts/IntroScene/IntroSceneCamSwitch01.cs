9f17f3527e4411adeacd743ff6d437d63f78993a
chrisb3546 <chrisb3546@yahoo.com>
2020-11-12 15:29:34 -0500

    finish opening scene

--- Assets/Scripts/IntroScene/IntroSceneCamSwitch01.cs
+++ Assets/Scripts/IntroScene/IntroSceneCamSwitch01.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17f1ae55e76fbfd8870e5bcbc11f1bc8c20475c9eb2992c10e60d9d362789974
+oid sha256:3832023b280b7a147709e557f1cd97ef09d6b946d1209dfb42f8d3beaf409d24
 size 606
--- Assets/Scripts/IntroScene/MoveCharInIntroScene.cs
+++ Assets/Scripts/IntroScene/MoveCharInIntroScene.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6db7e1a76ba1ebbe45c66dde99cbb9e524da3f5982a51d42095f780a9ea318c
-size 1137
+oid sha256:0a5bbfc35540080160f5b98ef2c2d30d4c9a323173e68a08892779f7a857ba3c
+size 1464
